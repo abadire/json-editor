2f161bb6bf90f45cb012a236f09d772911f5fb40
abadire <panvan97@gmail.com>
2021-01-15 18:34:53 +0300

    feat: add route data transfer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { TableComponent } from './components/table/table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
